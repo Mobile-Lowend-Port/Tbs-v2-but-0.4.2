fcff32af185665862ed9c716789d8fc6669124fd
Ajwyunsx <3427560235@qq.com>
2024-02-28 22:42:27 +0800

    haxe

---
+++ assets/preload/stages/Dside.lua
@@ -0,0 +1,9 @@
+function onCreate()
+	-- background shit
+
+	makeLuaSprite('Dside', 'Dside', -570, -60);
+	setLuaSpriteScrollFactor('Dside', 0.9, 0.9);
+	scaleObject('Dside', 0.83, 0.83);
+                addLuaSprite('Dside',false)
+
+end
