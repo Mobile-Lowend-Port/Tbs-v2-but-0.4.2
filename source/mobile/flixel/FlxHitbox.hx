26542eac31d1fdb26cf25a7e6551635979ceadf2
Ajwwkxkdang <107187521+Ajwyunsx@users.noreply.github.com>
2024-03-02 13:59:10 +0800

    Add files via upload

--- source/mobile/flixel/FlxHitbox.hx
+++ source/mobile/flixel/FlxHitbox.hx
@@ -88,16 +88,18 @@ class FlxHitbox extends FlxSpriteGroup
 		hint.immovable = true;
 		hint.scrollFactor.set();
 		hint.alpha = 0.00001;
-		hint.onDown.callback = hint.onOver.callback = function()
+		hint.onDown.callback = function()
 		{
-			if (hint.alpha != ClientPrefs.hitboxalpha)
-				hint.alpha = ClientPrefs.hitboxalpha;
+			if (hint.alpha != 0.2)
+				hint.alpha = 0.2;
 		}
-		hint.onUp.callback = hint.onOut.callback = function()
+		hint.onUp.callback = function()
 		{
 			if (hint.alpha != 0.00001)
 				hint.alpha = 0.00001;
 		}
+		hint.onOut.callback = hint.onUp.callback;
+		hint.onOver.callback = hint.onDown.callback;
 		#if FLX_DEBUG
 		hint.ignoreDrawDebug = true;
 		#end
