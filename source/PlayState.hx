487ef2d24d29758a8aadd96f5c8b717956b68e73
Ajwwkxkdang <107187521+Ajwyunsx@users.noreply.github.com>
2024-03-06 20:36:47 +0800

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -60,7 +60,7 @@ import StageData;
 import FunkinLua;
 import DialogueBoxPsych;
 import Conductor.Rating;
-import hscript.Conductor;
+//import hscript.Conductor;
 import hscript.Script;
 
 #if !flash 
