0998d887d1a5f6023ae6e579647881b34bf31659
Ajwwkxkdang <107187521+Ajwyunsx@users.noreply.github.com>
2024-03-07 19:39:31 +0800

    Add files via upload

--- assets/preload/data/sirokou/script.hx
+++ assets/preload/data/sirokou/script.hx
@@ -7,12 +7,37 @@ switch (curStep)
 	case 832:
 	PlayState.triggerEventNote('Image Flash', 'JS 3', '2');
 	case 896:
-	PlayState.triggerEventNote('Image Flash', 'JS 2', '0.2');
+	PlayState.triggerEventNote('Image Flash', 'JS 2', '0.3');
 	case 960:
 	PlayState.triggerEventNote('Image Flash', 'JS 3', '2');
 	case 990:
 	PlayState.triggerEventNote('Image Flash', 'JS 2', '0.3');
 	case 1021:
 	PlayState.triggerEventNote('Image Flash', 'JS 1', '0.3');
+	case 1792:
+	PlayState.triggerEventNote('Image Flash', 'JS 3', '2');
+	case 1854:
+	PlayState.triggerEventNote('Image Flash', 'JS 1', '0.3');
+	case 1856:
+	PlayState.triggerEventNote('Image Flash', 'JS 3', '2');
+	cass 1916:
+	PlayState.triggerEventNote('Image Flash', 'JS 2', '0.3');
+	case 1919:
+	PlayState.triggerEventNote('Image Flash', 'JS 3', '2');
+	case 1949:
+	PlayState.triggerEventNote('Image Flash', 'JS 1', '0.3');
+	case 1979:
+	PlayState.triggerEventNote('Image Flash', 'JS 2', '0.3');
+	case 1984:
+	PlayState.triggerEventNote('Image Flash', 'JS 3', '2');
+	case 2012:
+	PlayState.triggerEventNote('Image Flash', 'JS 2', '0.3');
+	case 2044:
+	JSchang(2, 0.3);
 	}
+}
+
+function JSchang(game, time)
+{
+PlayState.triggerEventNote('Image Flash', 'JS' + game, time);
 }
